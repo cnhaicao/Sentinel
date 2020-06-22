a3c9bf2cf24784119df15a1af63d13e67d5ec2d7
郭国强 <guoqiang.guo@ucarinc.com>
2020-06-22 15:59:53 +0800

    sentinel-dashboard 持久化到nacos

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
@@ -20,6 +20,7 @@ import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.api.config.ConfigFactory;
 import com.alibaba.nacos.api.config.ConfigService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,6 +32,8 @@ import java.util.List;
  */
 @Configuration
 public class NacosConfig {
+    @Value("${nacos.serverAddr}")
+    private String nacosServerAddr;
 
     @Bean
     public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
@@ -44,6 +47,6 @@ public class NacosConfig {
 
     @Bean
     public ConfigService nacosConfigService() throws Exception {
-        return ConfigFactory.createConfigService("localhost");
+        return ConfigFactory.createConfigService(nacosServerAddr);
     }
 }
